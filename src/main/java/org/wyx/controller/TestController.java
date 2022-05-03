3febe17ed53d9383f2637f71b707d4201a17079b
wyx-98 <18722689251@163.com>
2022-05-03 23:34:04 +0800

    init project

---
+++ src/main/java/org/wyx/controller/TestController.java
@@ -0,0 +1,14 @@
+package org.wyx.controller;
+
+/**
+ * @program: git-test
+ * @description: 测试controller
+ * @author: wyx
+ * @email: wyx9852@gmail.com
+ * @create: 2022-05-02 22:28
+ **/
+public class TestController {
+    public static void main(String[] args) {
+        //readmefsadf
+    }
+}
