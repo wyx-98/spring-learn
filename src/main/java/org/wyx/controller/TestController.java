aeb6cc05d033032e2a4512266b4d2180c274facb
wyx-98 <18722689251@163.com>
2022-05-30 14:33:20 +0800

    fix bug

--- src/main/java/org/wyx/controller/TestController.java
+++ src/main/java/org/wyx/controller/TestController.java
@@ -1,5 +1,11 @@
 package org.wyx.controller;
 
+import cn.hutool.core.date.DateBetween;
+import cn.hutool.core.date.DateUtil;
+import com.sun.tools.javac.util.List;
+
+import java.util.stream.Collectors;
+
 /**
  * @program: git-test
  * @description: 测试controller
@@ -9,6 +15,11 @@ package org.wyx.controller;
  **/
 public class TestController {
     public static void main(String[] args) {
-        //readmefsadf
+        List<String> list = List.of("www.baidu.com", "www.fasdfasdfsd.com", "ffdas.com");
+        java.util.List<String> com = list.stream()
+                .filter(string -> string.contains("com"))
+                .filter(string -> string.length() > 10)
+                .collect(Collectors.toList());
+
     }
 }
